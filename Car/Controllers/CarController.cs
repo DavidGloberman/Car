fd4a9b914d79b73cc6899994802fac6b8ded0c2b
02 <02@DESKTOP-A5DJ6R6>
2024-07-25 17:35:18 +0300

    Add project files.

---
+++ Car/Controllers/CarController.cs
@@ -0,0 +1,24 @@
+﻿using Car.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Model(string model, string year , string color)
+        {
+            var MyCar = new CarModel()
+            {
+                Model = string.IsNullOrEmpty(model) ? "N/A" : model,
+                Year = year,
+                Color = color
+            };
+            return View(MyCar);
+        }
+    }
+
+}
---
+++ Car/Models/CarModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Car.Models
+{
+    public class CarModel
+    {
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Color { get; set; }
+    }
+}
